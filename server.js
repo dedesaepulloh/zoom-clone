bcb72f2af0c609c87bf42a6e203d6857f0c49f97
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-21 11:24:56 +0700

    install ejs

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.status(200).send("Hay hay")
+    res.render('room');
 })
 
 
