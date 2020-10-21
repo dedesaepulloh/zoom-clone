c684bbd9a121c680accbec8234ad70bccd6c2f25
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-21 12:09:42 +0700

    Add the ability to view our own videos

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
+
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
